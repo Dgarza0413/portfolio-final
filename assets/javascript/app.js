76f802c51fcfe08ca991b7643faa310a8c443954
David Garza <dgarza0413@gmail.com>
2019-07-23 20:18:23 -0500

    removed assets

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("navbar");
+var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) / 2;
+
+function myFunction() {
+    if (window.pageYOffset > h) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+function scrollSection(scroll) {
+    var navItem = document.getElementById(scroll)
+    navItem.scrollIntoView();
+}
